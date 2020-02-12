d7ed9690eedd3692a25e461d73c0b7ebad5af056
umakvdu <umakvdu@gmail.com>
2020-02-12 15:42:45 +0530

    Venkatesh Ref FW

--- src/main/java/com/auto/test/Core/Base.java
+++ src/main/java/com/auto/test/Core/Base.java
@@ -21,9 +21,12 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Listeners;
+
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
 
+@Listeners(TestListners.class)
 public class Base {
 
 	@BeforeSuite
